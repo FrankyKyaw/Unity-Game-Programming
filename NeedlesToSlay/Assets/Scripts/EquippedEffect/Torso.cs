6a4974f0159132081cd152275eebff1d9132a515
FrankyKyaw <73188846+FrankyKyaw@users.noreply.github.com>
2020-11-04 19:20:43 +0630

    Checking in with Needles to Slay project

---
+++ NeedlesToSlay/Assets/Scripts/EquippedEffect/Torso.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CreatorKitCode;
+
+public class Torso : EquipmentItem.EquippedEffect
+{
+     private int AddedDefense = 100;
+     private int defense;
+     public override void Equipped(CharacterData user)
+     {
+        StatSystem.StatModifier modifier = new StatSystem.StatModifier();
+        modifier.ModifierMode = StatSystem.StatModifier.Mode.Absolute;
+        modifier.Stats.defense = AddedDefense;
+        user.Stats.AddModifier(modifier);
+     }
+     
+     public override void Removed(CharacterData user)
+     {
+        StatSystem.StatModifier modifier = new StatSystem.StatModifier();
+        modifier.ModifierMode = StatSystem.StatModifier.Mode.Absolute;
+        modifier.Stats.defense = -AddedDefense;
+        user.Stats.AddModifier(modifier);
+        
+     }
+}
