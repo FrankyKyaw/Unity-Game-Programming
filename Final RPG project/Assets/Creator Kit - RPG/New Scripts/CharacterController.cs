1a788a081a490be2474e28cd510731997a9d7785
FrankyKyaw <73188846+FrankyKyaw@users.noreply.github.com>
2020-12-01 16:46:44 +0630

    Checking in with final project

---
+++ Final RPG project/Assets/Creator Kit - RPG/New Scripts/CharacterController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPGM.Gameplay;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class CharacterController : MonoBehaviour
+{   
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+    public float speed;
+    public Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + horizontal * speed * Time.deltaTime;
+        position.y = position.y + vertical * speed * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+}
